b510534674260bd5328d6ec1508a04c2df98cdd8
Maddl <info@maddl.agency>
2026-03-17 12:30:32 +0100

    redirect fatti

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,50 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/contact',
+        destination: '/contatti',
+        permanent: true,
+      },
+      {
+        source: '/about',
+        destination: '/chi-siamo',
+        permanent: true,
+      },
+      {
+        source: '/cessione-del-quinto2dc9c957',
+        destination: '/cessione-del-quinto',
+        permanent: true,
+      },
+      {
+        source: '/delega-di-pagamento',
+        destination: '/delegazione-di-pagamento',
+        permanent: true,
+      },
+      {
+        source: '/i-nostri-servizi',
+        destination: '/cessione-del-quinto',
+        permanent: true,
+      },
+      {
+        source: '/cose-la-cessione-del-quinto',
+        destination: '/cessione-del-quinto',
+        permanent: true,
+      },
+      {
+        source: '/quando-potro-rinnovare-la-mia-cessione-del-quinto',
+        destination: '/cessione-del-quinto',
+        permanent: true,
+      },
+      {
+        source: '/prestiti-personali',
+        destination: '/soluzioni-liquidita',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
